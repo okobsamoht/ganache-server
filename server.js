232d8063c40678d340964c4cea7dce729b269509
Thomas Boko <thomas@right-com.com>
2021-08-17 20:27:10 +0100

    first

---
+++ server.js
@@ -0,0 +1,16 @@
+const ganache = require("ganache-core");
+require('dotenv').config();
+
+const options = {
+    mnemonic: "margin afraid simple man boss worth rose actress predict latin picture piano",
+    default_balance_ether: 1234567890,
+    total_accounts: 2,
+    // db_path: "https://ipfs.io/ipfs/QmWdBJ26yiVd34jfDkM5SswVUqCwjg1nHzpx3i3vPM6TB8",
+    // db_path: "/tmp/ganache-server",
+    logger: console
+}
+const server = ganache.server(options);
+server.listen(process.env.PORT || 8545, (a, b) => {
+    console.log(a);
+    console.log(b);
+});
